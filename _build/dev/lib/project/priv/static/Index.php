a18115e8c2ab16bc7c0dd3e2d482db5f68442809
JuanLoreiro <163009743+JuanLoreiro@users.noreply.github.com>
2024-03-19 20:28:05 -0300

    Add files via upload

---
+++ _build/dev/lib/project/priv/static/Index.php
@@ -0,0 +1,17 @@
+<?php
+
+#HTML PRINCIPAL
+include('loquesea/html/main.html');
+?>
+<link href="loquesea/css/styles.css" rel="stylesheet">
+<script type="text/javascript" src="loquesea/js/funciones.js"></script>
+<script type="text/javascript">
+
+
+
+	$(document).ready(function() {
+
+		/*ACA IRIA ALGO PUNTUAL DE JS */
+
+	})
+</script>
